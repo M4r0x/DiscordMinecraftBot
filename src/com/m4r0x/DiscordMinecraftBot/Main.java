75993b12b69ecfca17aa7c585af9bb4cf77d3a67
M4r0x <74778761+M4r0x@users.noreply.github.com>
2021-06-12 17:29:34 +0200

    First real test commit

--- src/com/m4r0x/DiscordMinecraftBot/Main.java
+++ src/com/m4r0x/DiscordMinecraftBot/Main.java
@@ -1,7 +1,10 @@
 package com.m4r0x.DiscordMinecraftBot;
 
+import net.dv8tion.jda.api.AccountType;
+import net.dv8tion.jda.api.JDABuilder;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
     }
 }
